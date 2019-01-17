17366251f12d04e3fb6b8bb2cd6a31184512aef9
micael Ståhl <micael_stahl96@hotmail.com>
2019-01-17 16:26:06 +0100

    Further progress on calculator (Close to done with loop in addition)

--- Assignment Calculator/Program.cs
+++ Assignment Calculator/Program.cs
@@ -14,33 +14,46 @@ namespace Assignment_Calculator
                     Console.Write("Enter the operator here  (+, -, *, /, % or x to exit): ");
                     string Operator = (Console.ReadLine() ?? "");
 
-                switch (Operator)
-                {
-                    case "+": //Additive
+                    switch (Operator)
+                    {
+                        case "+": //Additive
+
+                            RunAdditiveMethod();
                             double ResponseAdditive = RunAdditiveMethod();
-                        Console.WriteLine("The answer is: " + ResponseAdditive);
-                        break;
 
-                    case "-": //Subtraction
-                              double ResponseSubtraction = RunSubtractionMethod();
+                            Console.WriteLine("The answer is: " + ResponseAdditive);
+                            //
+                            break;
+
+                        case "-": //Subtraction
+                            double ResponseSubtraction = RunSubtractionMethod();
+
                             Console.WriteLine("The answer is: " + ResponseSubtraction);
-                        break;
+                            break;
 
-                    case "*": //Multiplication
-                              double ResponseMultiplication = RunMultiplicationMethod();
+                        case "*": //Multiplication
+                            double ResponseMultiplication = RunMultiplicationMethod();
+
                             Console.WriteLine("The answer is: " + ResponseMultiplication);
                             break;
 
-                    case "/": //Division
-                            //
-                              double ResponseDivision = RunDivisionMethod();
+                        case "/": //Division
+                                  //
+                            double ResponseDivision = RunDivisionMethod();
+                            if (ResponseDivision == 0)
+                            {
+                                Console.Clear();
+                                break;
+                            }
 
                             Console.WriteLine("The answer is: " + ResponseDivision);
-                        break;
+                            break;
 
-                    case "%": //Percentage
-                              //RunPercentageMethod();
-                        break;
+                        //case "%": //Percentage
+                        //          double ResponsePercentage = RunPercentageMethod();
+
+                        //        Console.WriteLine("The answer is: " + ResponsePercentage +"%");
+                        //    break;
 
                         case "x":
                             stayAlive = false;
@@ -52,19 +65,21 @@ namespace Assignment_Calculator
                             break;
 
                         default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("This is a invalid equationform!");
-                        break;
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("This is a invalid equationform!");
+                            break;
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("\nHit any key to continue!");
+                    Console.ReadKey();
+                    Console.Clear();
                 }
-                Console.ResetColor();
-                Console.WriteLine("Hit any key to continue!");
-                Console.ReadKey();
-                Console.Clear();
-            }
                 catch
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("That is not a valid equationform!");
+                    Console.ReadKey();
+                    Console.Clear();
                     Console.ResetColor();
                 }
             }
@@ -77,10 +92,67 @@ namespace Assignment_Calculator
             Console.Write("\n" + "Enter the next number here: ");
             double numberTwo = double.Parse(Console.ReadLine() ?? "");
 
-            double RunAdditiveMethod = numberOne + numberTwo;
+            double Answer = numberOne + numberTwo;
+            double RunAdditiveMethod = Answer;
 
+            Console.Write("\n" + "Would you like to add more numbers? y/n: ");
+            string Operator = (Console.ReadLine() ?? "");
+            switch (Operator)
+            {
+                case "y":
+                    if (Operator == "y")
+                        RunAdditiveMethodLoop(Answer);
+                    break;
+
+                case "n":
+                    if (Operator == "n")
+                        return RunAdditiveMethod;
+                    break;
+
+
+            }
             return RunAdditiveMethod;
+            }
+
+
+        private static double RunAdditiveMethodLoop(double Answer)
+        {
+
+            int i = 0;
+            do
+            {
+                Console.Write("\n" + "Enter the next number here: ");
+                double numberTwo = double.Parse(Console.ReadLine() ?? "");
+
+                Answer = Answer + numberTwo;
+
+                Console.Write("\n" + "The Answer is: " + Answer + " Want to add more numbers? y/n: ");
+                string Operator = (Console.ReadLine() ?? "");
+
+                if (Operator == "n")
+                {
+                    i = 1;
+                }
+            }
+            while (i < 1);
+            {
+                double RunAdditionMethod = Answer;
+                return RunAdditionMethod;
+                
+            }
+
+
+
+            //Console.Write("\n" + "Enter the next number here: ");
+            //double numberThree = double.Parse(Console.ReadLine() ?? "");
+
+            //double Answer1 = numberThree;
+
+            //double RunAdditiveMethodLoop = Answer + Answer1;
+            //return RunAdditiveMethodLoop;
         }
+
+
         private static double RunSubtractionMethod()
         {
             Console.Write("\n" + "Enter first number here: ");
@@ -93,6 +165,8 @@ namespace Assignment_Calculator
 
             return RunSubtractionMethod;
         }
+
+
         private static double RunMultiplicationMethod()
         {
             Console.Write("\n" + "Enter first number here: ");
@@ -105,6 +179,8 @@ namespace Assignment_Calculator
 
             return RunMultiplicationMethod;
         }
+
+
         private static double RunDivisionMethod()
         {
             Console.Write("\n" + "Enter first number here: ");
@@ -112,23 +188,44 @@ namespace Assignment_Calculator
 
             Console.Write("\n" + "Enter the next number here: ");
             double numberTwo = double.Parse(Console.ReadLine() ?? "");
+
             if (numberTwo == 0)
             {
+                double RunDivisionMethod = numberTwo;
+
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Oh oh! Can't divide by 0! Please try something else!");
+                Console.WriteLine("\nOh oh! Can't divide by 0! Please try something else!");
                 Console.ResetColor();
                 Console.ReadKey();
                 Console.Clear();
+
+                return RunDivisionMethod;
             }
-            double RunDivisionMethod = numberOne / numberTwo;
+            else
+            {
+                double RunDivisionMethod = numberOne / numberTwo;
 
-            return RunDivisionMethod;
-
-
-            //double RunDivisionMethod = numberOne / numberTwo;
-
-            //return RunDivisionMethod;
+                return RunDivisionMethod;
+            }
         }
+
+
+        //private static double RunPercentageMethod()
+        //{
+        //    Console.Write("\n" + "Enter first number here: ");
+        //    double numberOne = double.Parse(Console.ReadLine() ?? "");
+
+        //    Console.Write("\n" + "Enter the next number here: ");
+        //    double numberTwo = double.Parse(Console.ReadLine() ?? "");
+
+        //    double Percentage = numberOne - numberTwo;
+        //    //double RunPercentageMethod = (double)(Percentage / (double)numberOne) * 100;
+
+        //    //double RunPercentageMethod = (numberTwo / numberOne) * 100;
+        //    double RunPercentageMethod = (double)(numberTwo / (double)numberOne) * 100;
+
+        //    return RunPercentageMethod;
+        //}
     }
 
 }
